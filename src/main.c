e0a3a4864112ede2d5abb15c1f5fb9ebaebdb3f8
rodvfilho <rodvfilho@terra.com.br>
2020-04-27 20:55:07 -0300

    Added the control task to receive and enable the heater or humidifier.
    Added queues to communicate between the callback timer, the serial task and the control task.
    
    The callback timer send to the control task the sensor data.
    
    The serial task send to the control task the setpoint of temperature and humidity.
    
    The control task compare the values with the setpoint and enable or disable the heater or humidifier.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 /*********************************************************
-* Por: 
+* Por:
         Ageu Silva
-        Roberto
-        Paulo
+        Roberto Dalla Valle Filho
+        Paulo Paixão
 *********************************************************/
 
 
@@ -42,7 +42,7 @@
 
 /* definições para leitura de dados sensor DHT*/
 #define dhtDataPin GPIO_NUM_4
-#define sampleTime 1000  
+#define sampleTime 1000
 
 /*handle do Semaforo*/
 SemaphoreHandle_t xMutex;
@@ -51,7 +51,7 @@ SemaphoreHandle_t xMutex;
 /* Variáveis para Armazenar o handle da Task */
 TaskHandle_t xTaskRecSerial;
 
-/* timer handle */       
+/* timer handle */
 TimerHandle_t xTimer1;
 
 /* Protótipo das Tasks*/
@@ -66,9 +66,10 @@ void vSetup(void);
 typedef struct dht {
     uint32_t temperature;
     uint32_t humidity;
-    int8_t status;  
+    int8_t status;
 } dht_t;
-static dht_t sensor1;
+
+//static dht_t sensor1;
 
 
 /* Variável Global */
@@ -89,10 +90,10 @@ void vInitHW(void)
 
 /* Função app_main*/
 void app_main()
-{ 
+{
     vSetup();
 
-    while(true) 
+    while(true)
     {
         vTaskDelay(pdMS_TO_TICKS(3000));    /* Delay de 3 segundos */
     }
@@ -100,29 +101,52 @@ void app_main()
 
 
 /*Função setup - é chamada apenas uma vez na inicialização */
-void vSetup(void) 
+void vSetup(void)
 {
-  
+
     /* Configuração do Hardware */
-    vInitHW(); 
+    vInitHW();
 
     /* informações do Hardware */
-    esp_log_level_set(TAG1, ESP_LOG_INFO);       
+    esp_log_level_set(TAG1, ESP_LOG_INFO);
 
     /* Cria semaforo binário */
     xMutex = xSemaphoreCreateMutex();
-    
+
     /* comando necessário pois o semáforo começa em zero */
     xSemaphoreGive( xMutex );
+
+    //criação de fila do callBackTimer1
+  	xMonitor = xQueueCreate( 10, sizeof( int ) );
+
+    if(xMonitor == NULL){
+  		ESP_LOGI("Erro na criação da Queue.\n");
+  	}
+
+    //criação de fila do Serial
+  	xSerial = xQueueCreate( 10, sizeof( int ) );
+
+    if(xSerial == NULL){
+  		ESP_LOGI("Erro na criação da Queue.\n");
+  	}
 
     //xTaskCreate(vPiscarLED,Nome da Task,Stack Size,parametro passado para a task,Prioridade da task,handle da task);
     if( xTaskCreate( &vTaskRecSerial, "Task Rec Serial", 2048, NULL, 1, xTaskRecSerial )!= pdTRUE )
     {
         if( DEBUG )
         {
-            ESP_LOGI( TAG1, "error - nao foi possivel alocar task_Contador.\n" ); 
+            ESP_LOGI( TAG1, "error - nao foi possivel alocar task_Contador.\n" );
         }
-        return;     
+        return;
+    }
+
+    if( xTaskCreate( &vTaskControl, "TaskControl", 2048, NULL, 1, xTaskControl )!= pdTRUE )
+    {
+        if( DEBUG )
+        {
+            ESP_LOGI( TAG1, "error - nao foi possivel alocar task_Contador.\n" );
+        }
+        return;
     }
 
     /* cria e testa auto-reload timer 1 */
@@ -137,42 +161,146 @@ void vSetup(void)
         #endif
         /* timer1 start */
         xTimerStart( xTimer1, 0 );
-    } 
+    }
 
 }
 
 /* timer para amostragem cíclica de dados do sensor DHT11 */
 void callBackTimer1( TimerHandle_t pxTimer ) {
-	
+
+    dht_t sensorSent;
+
     /* atribui pino de dados de entrada do sensor */
     DHT11_init(dhtDataPin);
 
     /* leitura de dados do sensor dht */
-    sensor1.temperature = DHT11_read().temperature;
-    sensor1.humidity = DHT11_read().humidity;
-    sensor1.status = DHT11_read().status;
+    sensorRead.temperature = DHT11_read().temperature;
+    sensorRead.humidity = DHT11_read().humidity;
+    sensorRead.status = DHT11_read().status;
 
-    /* exibe dados */
-    #if(DEBUG)
-        printf( "Temperatura: %d\n", sensor1.temperature );
-        printf( "Humidade: %d\n", sensor1.humidity );
-        printf("Status: %d\n", sensor1.status);
-    #endif
+    if(!xQueueSend(xMonitor, &sensorSent, 500)) {
+      ESP_LOGI("Falha ao enviar o valor para a fila dentro de 500ms.\n");
+    }
+
+    if(!xQueueSend(xSerial, &sensorSent, 500)) {
+      ESP_LOGI("Falha ao enviar o valor para a fila dentro de 500ms.\n");
+    }
+
+/*    #if(DEBUG)
+
+        //exibe dados
+        printf( "Temperatura: %d\n", sensorRead.temperature );
+        printf( "Umidade: %d\n", sensorRead.humidity );
+        printf("Status: %d\n", sensorRead.status);
+
+      #endif
+
+
+*/
+
 }
+
+
+/* Task de controle: Responsável por habilitar/desabilitar aquecedor e/ou umidificador */
+
+void vTaskControl( void *pvParameter )
+{
+
+  dht_t sensorReceived; //struct local para receber valor da struct dht_t sensorSent
+  uint8_t setPoint[3];  //variavel local para receber da fila xSerial a temperatura ou umidade
+
+  uint8_t temp = 0;     //variavel local para receber setPoint temperatura
+  uint8_t umid = 0;     //variavel local para receber setPoint umidade
+
+
+
+
+
+  while(true){
+
+    if(!xQueueReceive(xSerial, &setPoint, 200)) {
+      ESP_LOGI("Falha ao receber o valor da fila dentro de 2500ms.\n");
+    }
+
+    if (!xQueueReceive(xMonitor, &sensorReceived, 2500)) {
+      ESP_LOGI("Falha ao receber o valor da fila dentro de 2500ms.\n");
+    }
+
+    switch (setPoint[0]) {
+
+      case 'T':
+        temp = atoi(setPoint[1]); //valor convertido de string para int
+        break;
+
+      case 'U':
+        umid = atoi(setPoint[1]); //valor convertido de string para int
+        break;
+
+    }
+
+    if (temp != 0) {
+      if (sensorReceived.temperature > temp) {
+          /*ligar GPIO Umidificador*/
+          //configurar GPIO
+          /*envia ON Umidificador : xQueueSend para a fila xSerial */
+          if(!xQueueSend(xSerial, "UON", 200)) {
+            ESP_LOGI("Falha ao enviar o valor para a fila dentro de 500ms.\n");
+          }
+
+      }
+      else {
+        /*desligar GPIO Umidificador*/
+        //configurar GPIO
+        /*envia OFF Umidificador : xQueueSend para a fila xSerial */
+        if(!xQueueSend(xSerial, "UOFF", 200)) {
+          ESP_LOGI("Falha ao enviar o valor para a fila dentro de 500ms.\n");
+        }
+      }
+
+    }
+
+    if (umid != 0) {
+      if (sensorReceived.humidity > umid) {
+          /*ligar GPIO Aquecedor*/
+          //configurar GPIO
+          /*envia ON Aquecedor : xQueueSend para a fila xSerial */
+          if(!xQueueSend(xSerial, "AON", 200)) {
+            ESP_LOGI("Falha ao enviar o valor para a fila dentro de 500ms.\n");
+          }
+
+      }
+      else {
+        /*desligar GPIO Aquecedor*/
+        //configurar GPIO
+        /*envia OFF Aquecedor : xQueueSend para a fila xSerial */
+        if(!xQueueSend(xSerial, "AOFF", 200)) {
+          ESP_LOGI("Falha ao enviar o valor para a fila dentro de 500ms.\n");
+        }
+
+      }
+
+    }
+
+  }
+
+}
+
+
 
 /*===========================================================================================*/
 // Função que recebe os dados da serial USB e trata as informações de protocolo
 
 void vTaskRecSerial( void *pvParameter )
 {
+
     (void) pvParameter;
 
     char *dadosNameTask = pcTaskGetTaskName(xTaskRecSerial);
     if( DEBUG )
     {
-        ESP_LOGI( TAG1, "%s \n", dadosNameTask); 
+        ESP_LOGI( TAG1, "%s \n", dadosNameTask);
     }
-    
+
     uart_config_t uart_config = {
         .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
@@ -181,20 +309,20 @@ void vTaskRecSerial( void *pvParameter )
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
     };
     uart_param_config(UART_NUM_0, &uart_config);
-    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);    
+    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
 
     while (true)
     {
         len = 0;
         len = uart_read_bytes(UART_NUM_0, cBufferRx_USB, BUF_SIZE, 20 / portTICK_RATE_MS);
-        
+
         if(len > 0)
         {
             strcpy((char*)cBufferRx_USB_Comp, (char*)cBufferRx_USB);
 
             if(strncmp((char*)cBufferRx_USB_Comp, (char*)"[SETTEMP=", strlen((char*)"[SETTEMP=")) == false)
             {
-                
+
                 uint8_t cSetTemp[3];
 
                 cSetTemp[0] = 'T';
@@ -203,9 +331,13 @@ void vTaskRecSerial( void *pvParameter )
 
                 strcpy((char*)cBufferTx_USB, (char*)cSetTemp);
 
-                // Aqui entra a função da fila. Por favor, comente o codigo abaixo
+                if(!xQueueSend(xSerial, &cSetTemp, 500)) {
+                  ESP_LOGI("Falha ao enviar o valor para a fila dentro de 500ms.\n");
+                }
                 //uart_write_bytes(UART_NUM_0, (char*)cBufferTx_USB, strlen((char*)cBufferTx_USB));
                 //uart_write_bytes(UART_NUM_0, (char*)"\r\n", strlen((char*)"\r\n"));
+
+
             }
 
             else if(strncmp((char*)cBufferRx_USB_Comp, (char*)"[SETUMID=", strlen((char*)"[SETUMID=")) == false)
@@ -217,15 +349,21 @@ void vTaskRecSerial( void *pvParameter )
                 cSetUmi[2] = cBufferRx_USB_Comp[10];
 
                 strcpy((char*)cBufferTx_USB, (char*)cSetUmi);
-                uart_write_bytes(UART_NUM_0, (char*)cBufferTx_USB, strlen((char*)cBufferTx_USB));
-                uart_write_bytes(UART_NUM_0, (char*)"\r\n", strlen((char*)"\r\n"));
+
+                if(!xQueueSend(xSerial, &cSetUmi, 500)) {
+                  ESP_LOGI("Falha ao enviar o valor para a fila dentro de 500ms.\n");
+                }
+
+                //uart_write_bytes(UART_NUM_0, (char*)cBufferTx_USB, strlen((char*)cBufferTx_USB));
+                //uart_write_bytes(UART_NUM_0, (char*)"\r\n", strlen((char*)"\r\n"));
+
             }
 
             else{
                 memset((char*)cBufferRx_USB_Comp, '\0', strlen((char*)cBufferRx_USB_Comp));
             }
         }
-    }       
+    }
 }
 
 /*===========================================================================================*/
